fd45a4682c0c968eb4dcdfccfc0f7b604b196eb5
ccg8e <christinegraff@Christines-MacBook-Pro.local>
2013-12-09 16:30:33 +0100

    ldkjflsjf

---
+++ teseos corrections/CCanvas.cpp
@@ -0,0 +1,216 @@
+#include "CCanvas.h"
+#include "Base.h"
+#include "Sphere.h"
+#include "Sun.h"
+#include "planet.h"
+#include <QString>
+#include <QImage>
+
+using namespace std;
+
+//-----------------------------------------------------------------------------
+
+void CCanvas::initializeGL()
+{
+
+  glClearColor(0.0f, 0.0f, 0.0f, 0.5f);			          // Black Background
+  glClearDepth(1.0f);									                // Depth Buffer Setup
+  glEnable(GL_DEPTH_TEST);							              // Enables Depth Testing
+  glDepthFunc(GL_LEQUAL);								              // The Type Of Depth Testing To Do
+  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+  HourOfDay = 0.0;
+  DayOfYear = 0.0;
+  AnimateIncrement = 1.0;
+
+  sun=new Sun();
+};
+
+
+//-----------------------------------------------------------------------------
+
+
+void  CCanvas::glPerspective(
+        const GLdouble fovy,
+        const GLdouble aspect,
+        const GLdouble zNear,
+        const GLdouble zFar)
+{
+
+
+    const GLdouble d=1.0/tan(fovy/360.0*PI);
+    const GLdouble delta=zNear-zFar;
+
+    GLdouble *mat=new GLdouble[16];
+
+    mat[0]=d/aspect;
+    mat[1]=0;
+    mat[2]=0;
+    mat[3]=0;
+
+    mat[4]=0;
+    mat[5]=d;
+    mat[6]=0;
+    mat[7]=0;
+
+    mat[8]=0;
+    mat[9]=0;
+    mat[10]=(zNear+zFar)/delta;
+    mat[11]=-1;
+
+    mat[12]=0;
+    mat[13]=0;
+    mat[14]=2*zNear*zFar/delta;
+    mat[15]=0;
+
+
+    glMultMatrixd(mat);
+
+    delete[] mat;
+}
+
+
+void CCanvas::  lookAt(const GLdouble eyex,
+                       const GLdouble eyey,
+                       const GLdouble eyez,
+                       const GLdouble centerx,
+                       const GLdouble centery,
+                       const GLdouble centerz,
+                       const GLdouble upx,
+                       const GLdouble upy,
+                       const GLdouble upz)
+{
+
+
+    GLdouble *mat=new GLdouble[16];
+
+    Point3d p(centerx, centery, centerz);
+        Point3d eye(eyex, eyey, eyez);
+        Point3d vup(upx, upy, upz);
+        Point3d vpn = eye-p;
+        vpn.normalize();
+
+        Point3d s = vup^vpn;
+
+        mat[0] = s.x();
+        mat[1] = upx;
+        mat[2] = vpn.x();
+        mat[3] = 0;
+
+        mat[4] = s.y();
+        mat[5] = upy;
+        mat[6] = vpn.y();
+        mat[7] = 0;
+
+        mat[8] = s.z();
+        mat[9] = upz;
+        mat[10] = vpn.z();
+        mat[11] = 0;
+
+        mat[12] = eyex;
+        mat[13] = eyey;
+        mat[14] = eyez;
+        mat[15] = 1;
+
+    glMultMatrixd(mat);
+
+    delete[] mat;
+
+
+}
+
+void CCanvas::resizeGL(int width, int height)
+{
+  // set up the window-to-viewport transformation
+  glViewport( 0,0, width,height );
+
+  // vertical camera opening angle
+  double beta = 90.0;
+
+  // aspect ratio
+  double gamma;
+  if (height > 0)
+    gamma = width/(double)height;
+  else
+    gamma = width;
+
+  // front and back clipping plane at
+  double n = -1.0;
+  double f = -100.0;
+  
+  // frustum corners
+  double t = -tan(beta*3.14159/360.0) * n;
+  double b = -t;
+  double r = gamma * t;
+  double l = -r;
+
+  // set projection matrix
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glFrustum( l,r , b,t , -n,-f );
+
+  // alternatively, directly from alpha and gamma
+//  glPerspective( beta, gamma, -n, -f );
+}
+
+//-----------------------------------------------------------------------------
+
+void CCanvas::paintGL()
+{
+    glEnable(GL_TEXTURE_2D);
+
+    if(sun==NULL) return;
+
+//   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // clear screen and depth buffer
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glShadeModel(GL_SMOOTH);
+  //glEnable(GL_COLOR_MATERIAL);
+  // set model-view matrix
+
+  glMatrixMode (GL_PROJECTION);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  glPushMatrix();
+  lookAt( 0,0,0,  0,0,-1,  0,1,0 );     // camera position , "look at" point , view-up vector
+//  glTranslatef(0.0, 0.0, -8.0);
+
+  //glRotatef(15.0, 1.0, 0.0, 0.0);
+
+  GLfloat ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+  GLfloat diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+  GLfloat specular[] = { 1.0, 1.0, 1.0, 1.0 };
+  GLfloat position[] = { 0.0, 4, -5, 1.0 };
+
+  GLfloat position1[] = { 0.0, 0, 0, 1 };
+
+  glLightfv( GL_LIGHT0, GL_AMBIENT, ambient );
+  glLightfv( GL_LIGHT0, GL_DIFFUSE, diffuse );
+  glLightfv( GL_LIGHT0, GL_SPECULAR, specular );
+  glLightfv( GL_LIGHT0, GL_POSITION, position );
+  glEnable( GL_LIGHTING );
+  glEnable( GL_LIGHT0 );
+
+
+  glLightfv( GL_LIGHT1, GL_AMBIENT, ambient );
+  glLightfv( GL_LIGHT1, GL_DIFFUSE, diffuse );
+  glLightfv( GL_LIGHT1, GL_SPECULAR, specular );
+  glLightfv( GL_LIGHT1, GL_POSITION, position1 );
+  glEnable( GL_LIGHTING );
+  glEnable( GL_LIGHT1 );
+
+//  cout << "got here"<<endl;
+
+
+
+
+
+  sun->draw(HourOfDay, DayOfYear);
+
+  HourOfDay += AnimateIncrement;
+  DayOfYear = HourOfDay;
+  //tau += 1.0;
+  //tau2 += 0.1;
+}
---
+++ teseos corrections/sun.cpp
@@ -0,0 +1,77 @@
+#include "Sun.h"
+#include "earth.h"
+#include "moon.h"
+#include "Base.h"
+#include "Sphere.h"
+#include <GLUT/glut.h>
+
+
+
+
+
+
+Sun::Sun()
+{
+
+    glEnable(GL_TEXTURE_2D);
+
+//    children.insert(children.end(),(Planet)earth);
+//    children.insert(children.end(), (Planet)moon);
+
+    QImage img;
+    if( !img.load("/Users/christinegraff/Documents/OpenGL_Framework 2/sun.bmp")){
+        qWarning("ERROR LOADING IMAGE");
+    }
+
+    //img.save("/Users/christinegraff/Documents/OpenGL_Framework 2/asdasd.bmp");
+
+    //img.save("asd");
+    //cout<< img << endl;
+    img = QGLWidget::convertToGLFormat(img);
+    glGenTextures(1, &name);
+    glBindTexture(GL_TEXTURE_2D, name);
+    glTexImage2D(GL_TEXTURE_2D,0, GL_RGBA, img.width(), img.height(), 0,GL_RGBA, GL_UNSIGNED_BYTE,img.bits());
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+
+//    glEnable(GL_TEXTURE_2D);
+//    glBindTexture(GL_TEXTURE_2D, name);
+
+
+
+    //glBindTexture(GL_TEXTURE_2D, name);
+
+
+//    GLenum err;
+//        while ((err = glGetError()) != GL_NO_ERROR) {
+//            cerr << "OpenGL error: " << err << endl;
+//        }
+
+    children.push_back(new Earth());
+}
+
+
+
+void Sun::draw(float HourOfDay, float DayOfYear){
+    //glPushMatrix();
+
+    glTranslated (0.0, 0.0, -5.0);
+
+    glPushMatrix();
+    glScaled (2.0, 2.0, 2.0);
+//    glColor3f(1.0, 1.0, 0.0);
+    glBindTexture(GL_TEXTURE_2D, name);
+    sphere.draw();
+    glPopMatrix();
+
+
+    for (int i = 0; i <children.size(); i++){
+        children[i]->draw(HourOfDay, DayOfYear);
+    }
+    //create the children
+//
+//    earth.draw(HourOfDay, DayOfYear);
+
+}
